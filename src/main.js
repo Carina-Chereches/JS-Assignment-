694958176792d87c299b3c0dfcc964d56d7b0c39
Carina-Chereches <ch.carinamaria@gmail.com>
2016-07-19 14:37:48 +0300

    display directories tree and display filter

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
-const folders = directories: [
+
+
+
+var folders = 
       {
         type: 'dir',
         name: 'app',
@@ -55,4 +58,55 @@ const folders = directories: [
           }
         ]
       }
-    ]
+
+function tree(data ) {
+   var arr = "<ul class='folder-container'>";
+
+  for (var key in data) {
+    if (typeof(data[key]) == 'object' && data[key] != null) {
+      arr += "<li class='folder-wrapper'><ul class='folder-container>'>"+ tree(data[key])+"</ul></li>";
+      arr += '</li>';
+    } else {
+      if (key==='name' && data['type']!=='dir')
+        arr += ("<li class='file-item'>" + ' &quot;' + data[key] + '&quot;</li>' );
+      else if (data[key] === 'dir')
+        arr += "<li class='folder-item'>" + data['name'] + "</li>";
+    }
+  };
+
+   arr += '</ul >';
+  return( arr );
+}
+
+document.getElementById('myDiv').innerHTML=tree(folders);
+
+
+
+
+
+
+// var input = document.getElementById("input");
+// input.onkeyup = function () {
+//   var res=[];
+//   var list = document.getElementsByTagName('li');
+//   console.log(list);
+//
+//
+//   for (var i = 0; i < list.length; i++) {
+//     if (list[i].textContent.indexOf(input.value) < 0) {
+//       list[i].style.display = 'none';
+//       //list[i+1].style.display = 'list-item';
+//     }
+//   }
+// }
+//
+
+
+
+
+
+
+
+
+
+
